90b76de47ab3b7f72b8e67ce0d7fc504d0f888a5
Akin Aguda <lordtitan7@gmail.com>
2020-04-04 04:23:00 +0100

    works fine

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,12 +1,17 @@
 pub mod functions {
     use super::super::algorithms;
     use algorithms::algorithms::{Node, ValueTypes};
+    use std::fs;
     pub fn read_from_file(url: &str) -> String {
-        use std::fs;
         fs::read_to_string(url).unwrap()
     }
+    pub fn write_to_file(path: &str, content: &str) {
+        use std::io::Write;
+        let file_path = std::path::Path::new(path);
+        let mut file = fs::File::create(file_path).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+    }
     pub fn get_unique_chars(text: &str) -> Vec<Box<Node>> {
-        use std::collections::HashMap;
         let mut map: HashMap<char, usize> = HashMap::new();
         let mut nodes: Vec<Box<Node>> = vec![];
         for (_, c) in text.chars().enumerate() {
@@ -32,11 +37,12 @@ pub mod functions {
             Box::new(Node::new(ValueTypes::Character('k'), Some(2))),
             Box::new(Node::new(ValueTypes::Number(5), Some(15))),
         ];
+        assert_eq!(sort_nodes(&mut test_vector)[0].get_freq(), 15);
         assert_eq!(sort_nodes(&mut test_vector)[3].get_freq(), 2);
     }
-    pub fn build_huffman(list: &mut Vec<Box<Node>>) -> &Node {
+    pub fn build_huffman(list: &mut Vec<Box<Node>>) -> Box<Node> {
         if list.len() > 1 {
-            let sum = list[0].get_freq() + list[1].get_freq();
+            let sum = list[list.len() - 1].get_freq() + list[list.len() - 2].get_freq();
             let mut new_root = Node::new(ValueTypes::Number(sum), Some(sum));
             let a = list.pop();
             let b = list.pop();
@@ -46,7 +52,48 @@ pub mod functions {
             let list = sort_nodes(list);
             build_huffman(list)
         } else {
-            &list[0]
+            list.pop().unwrap()
         }
+    }
+    use std::collections::HashMap;
+    pub fn get_map(node: &Box<Node>, map: &mut HashMap<char, String>, bin_string: String) {
+        if let ValueTypes::Character(value) = node.value {
+            map.insert(value, bin_string);
+        } else {
+            if let Some(ref left) = node.left {
+                get_map(left, map, bin_string.clone() + "0");
+            }
+            if let Some(ref right) = node.right {
+                get_map(right, map, bin_string.clone() + "1");
+            }
+        }
+    }
+    pub fn encode(characters: &str, map: &HashMap<char, String>) -> String {
+        let mut encoded = String::new();
+        for val in characters.chars() {
+            let v = map.get(&val);
+            encoded.push_str(v.unwrap());
+        }
+        encoded
+    }
+    pub fn decode(value: &str, root: &Box<Node>) -> String {
+        let mut decoded = String::new();
+        let mut root_ptr = root;
+        for x in value.chars() {
+            if x == '0' {
+                if let Some(ref l) = root_ptr.left {
+                    root_ptr = l;
+                }
+            } else {
+                if let Some(ref r) = root_ptr.right {
+                    root_ptr = r;
+                }
+            }
+            if let ValueTypes::Character(ch) = root_ptr.value {
+                decoded.push(ch);
+                root_ptr = root;
+            }
+        }
+        decoded
     }
 }
